24898002743c7732cfd5cf659a9017024c6c16cc
lorenzo-andreaus <lorenzoandreaus@gmail.com>
2025-10-07 01:19:45 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,12 +1,11 @@
 function menu(no)
 {
-  link = new Array("index.html", "publi.html", "teaching.html", "admin.html", "links.html", "perso.html")
-  onglet = new Array('Accueil', 'Recherche', 'Enseignement', 'Administration', 'Liens', 'Perso')
-  title = new Array('Home', 'Research', 'Teaching', '', 'Links', '')
+  link = new Array("index.html", "research.html", "talks.html", "links.html", "personal.html")
+  title = new Array('Home', 'Research', 'talks', 'Links', 'Beyond Mathematics')
   document.write('<div class="cadre">')
   document.write('<nav>')
   document.write('<ul>')
-  for( var i = 0; i < 6; ++i)
+  for( var i = 0; i < 5; ++i)
   {
     if (i == no)
       document.write('<li><div title="' + title[i] + '" class="here">' + title[i] + '</div></li>')
@@ -14,6 +13,5 @@ function menu(no)
       document.write('<li><a title="' + title[i] + '" href="' + link[i] + '">' + title[i] + '</a></li>')
   }
   document.write('</ul></nav>')
-  document.write('<a href="https://www.math.univ-toulouse.fr/"><img title="Institut de Mathématiques de Toulouse" alt="Logo IMT" src="pictures/logo_IMT.jpg" class="logo"></a>')
   document.write('</div>')
 }
