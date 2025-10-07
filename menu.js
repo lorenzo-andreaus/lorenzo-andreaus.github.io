c4a55c08a990a8b4a5f96f8c22ebe589c30a1edc
lorenzo-andreaus <lorenzoandreaus@gmail.com>
2025-10-07 14:36:04 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,17 +1,1 @@
-function menu(no)
-{
-  link = new Array("index.html", "research.html", "personal.html")
-  title = new Array('Home', 'Research', 'Beyond Mathematics')
-  document.write('<div class="cadre">')
-  document.write('<nav>')
-  document.write('<ul>')
-  for( var i = 0; i < 3; ++i)
-  {
-    if (i == no)
-      document.write('<li><div title="' + title[i] + '" class="here">' + title[i] + '</div></li>')
-    else 
-      document.write('<li><a title="' + title[i] + '" href="' + link[i] + '">' + title[i] + '</a></li>')
-  }
-  document.write('</ul></nav>')
-  document.write('</div>')
-}
+
